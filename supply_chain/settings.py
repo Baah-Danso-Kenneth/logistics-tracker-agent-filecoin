d7077f761ad9e15fc46995b205e0791b7c5d36b8
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-03-26 20:49:43 +0000

    Codebase Refactor

--- supply_chain/settings.py
+++ supply_chain/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #Local Apps
-    'trackings.apps.TrackingsConfig',
+    'delivery.apps.DeliveryConfig',
 
     #Third Party Apps
     'rest_framework',
--- trackings/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from trackings.models import Shipment
-
-
-@admin.register(Shipment)
-class ShipmentAdmin(admin.ModelAdmin):
-    list_display = ('tracking_id', 'origin', 'destination', 'status', 'created_at')
